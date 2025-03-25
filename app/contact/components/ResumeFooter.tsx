9a2cb0d51867af9441b9129d386e623aa763027f
Deval Panchal <devalp2401@gmail.com>
2025-03-25 17:28:42 -0400

    added resume

--- app/contact/components/ResumeFooter.tsx
+++ app/contact/components/ResumeFooter.tsx
@@ -17,9 +17,14 @@ export default function ResumeFooter() {
         >
 			<header className="flex items-center justify-center max-w-5xl mx-auto py-8 px-4 sm:px-6 lg:px-8 bg-[#1d1d1f] rounded-lg">
 				<h1 className="font-medium text-4xl w-2/3 text-white">View my Resume</h1>
-				<button className="border-2 border-gray-400 font-bold hover:bg-[#0a66c2] hover:border-[#0a66c2] hover:text-white text-white px-6 py-2 rounded-full flex items-center gap-2 mx-auto transition-colors">
+				<a 
+					href="/resume/Deval_Panchal_Resume.pdf"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="border-2 border-gray-400 font-bold hover:bg-[#0a66c2] hover:border-[#0a66c2] hover:text-white text-white px-6 py-2 rounded-full flex items-center gap-2 mx-auto transition-colors"
+				>
 					Download Resume
-				</button>
+				</a>
 			</header>
 		</motion.div>
 	);
--- app/resume/components/Resume.tsx
+++ app/resume/components/Resume.tsx
@@ -5,9 +5,14 @@ export default function ResumeHeader() {
 		<div className="w-full bg-gray-200/30 border-b">
 			<header className="flex items-center justify-center max-w-5xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
 				<h1 className="font-medium text-4xl w-2/3">Resume</h1>
-				<button className="border-2 border-gray-400 font-bold hover:bg-gray-400 hover:text-white text-gray-700 px-6 py-2 rounded-full flex items-center gap-2 mx-auto transition-colors">
+				<a 
+					href="/resume/Deval_Panchal_Resume.pdf"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="border-2 border-gray-400 font-bold hover:bg-gray-400 hover:text-white text-gray-700 px-6 py-2 rounded-full flex items-center gap-2 mx-auto transition-colors"
+				>
 					Download as PDF
-				</button>
+				</a>
 			</header>
 		</div>
 	);
